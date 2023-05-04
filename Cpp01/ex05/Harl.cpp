109ec8d55d7857cb8eaa62a9341e88ef8689fec1
Eyward <zakarifor77@gmail.com>
2023-05-04 04:33:22 +0100

    cleaning up

--- Cpp01/ex05/Harl.cpp
+++ Cpp01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: zzirh <zzirh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 03:17:46 by zzirh             #+#    #+#             */
-/*   Updated: 2023/05/04 04:19:25 by zzirh            ###   ########.fr       */
+/*   Updated: 2023/05/04 04:31:15 by zzirh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,31 @@
 
 Harl::Harl()
 {
-    std::cout << "Constructir : create Harl" << std::endl;
+    std::cout << "Constructor : create Harl\n" << std::endl;
 }
 
 Harl::~Harl()
 {
-    std::cout << "Destructor : destroy Harl" << std::endl;
+    std::cout << "\nDestructor : destroy Harl" << std::endl;
 }
 
 void    Harl::debug( void)
 {
-    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger\n";
+    std::cout << " I really do!" << std::endl;
 }
 
 void    Harl::info( void)
 {
     std::cout << "I cannot believe adding extra bacon costs more money" << std::endl;
+    std::cout << "You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl; 
+
 }   
 
 void    Harl::warning( void)
 {
     std::cout << "I think I deserve to have some extra bacon for free" << std::endl;
+    std::cout << " I’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void    Harl::error( void)
